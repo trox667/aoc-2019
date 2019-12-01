e306dc5575ccca74fa0e9192a39a792dc5cdf9f0
trox667 <trox667@googlemail.com>
2019-12-01 08:48:23 +0100

    day1 second star

--- day1/rust_1/src/main.rs
+++ day1/rust_1/src/main.rs
@@ -1,10 +1,15 @@
 use baselib::io;
 
 fn main() {
+    println!("🎅 Happy Advent of Code, let's start 🎁...");
     let sum = get_fuel_amount(io::read_file("input"));
     println!("total amount of fuel {}", sum);
+    let fuel = get_total_fuel_amount(io::read_file("input"));
+    println!("total amount of fuel with mass {}", fuel);
+    println!("🎅 ...done for today 🎄");
 }
 
+// part 1
 fn get_fuel_amount(lines: Vec<String>) -> usize {
     lines.iter().filter_map(|line| {
        parse_mass(line) 
@@ -17,7 +22,29 @@ fn parse_mass(s: &str) -> Option<usize> {
 
 fn mass_to_fuel(mass: usize) -> usize {
    let mass = mass as f32;
-   ((mass / 3.0).floor() - 2.0) as usize
+   match (mass / 3.0).floor() - 2.0 {
+       fuel if fuel > 0.0 => fuel as usize,
+       _ => 0
+   }
+}
+
+// part 2
+
+fn get_total_fuel_amount(lines: Vec<String>) -> usize {
+    lines.iter().filter_map(|line| {
+       parse_mass(line) 
+    }).map(mass_to_fuel_add_fuel).sum()
+}
+
+fn mass_to_fuel_add_fuel(mass: usize) -> usize {
+    let mut fuel = mass_to_fuel(mass);
+    let mut sum = fuel;
+    while mass_to_fuel(fuel) > 0 {
+        fuel = mass_to_fuel(fuel);
+        sum += fuel;
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -42,4 +69,21 @@ mod test {
         let d = d.into_iter().map(String::from).collect();
         assert_eq!(get_fuel_amount(d), 34241);
     }
+
+    #[test]
+    fn star_one() {
+        let sum = get_fuel_amount(io::read_file("input"));
+        assert_eq!(sum, 3327415);
+    }
+
+    #[test]
+    fn fuel_for_fuel() {
+        let fuel = mass_to_fuel_add_fuel(12);
+        assert_eq!(fuel, 2);
+        let fuel = mass_to_fuel_add_fuel(1969);
+        assert_eq!(fuel, 966);
+        let fuel = mass_to_fuel_add_fuel(100756);
+        assert_eq!(fuel, 50346);
+    }
+
 }
