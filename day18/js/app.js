161ed5028b2974119c7e468dc656b39af9e509d8
Malte Rieken <malte.rieken@techniatranscat.com>
2019-12-19 11:33:24 +0100

    (wip) js solution for day18

--- day18/js/app.js
+++ day18/js/app.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
-const readline = require("readline");
+const fs = require('fs');
+const readline = require('readline');
 
 const readInterface = readline.createInterface({
-  input: fs.createReadStream("../input")
+  input: fs.createReadStream('../input'),
 });
 
 let lines = [];
 
-readInterface.on("line", line => lines.push(line.trim()));
+readInterface.on('line', line => lines.push(line.trim()));
 
-readInterface.on("close", () => {
+readInterface.on('close', () => {
   main(lines);
 });
 
@@ -42,7 +42,7 @@ const addToSet = (visited, [x, y]) => {
   return visited;
 };
 
-// DFS search of items in the map starting at 
+// DFS search of items in the map starting at
 // a given position
 const search = (map, startPosition, target) => {
   let found = false;
@@ -65,7 +65,7 @@ const search = (map, startPosition, target) => {
         const y = cy + dy;
         const v = map[y][x];
         if (v === target) {
-          result.push([x, y, cs, [...foundItems]]);
+          result = [x, y, cs, [...foundItems]];
           found = true;
         } else if (v === WALL) {
           visited = addToSet(visited, [x, y]);
@@ -80,7 +80,7 @@ const search = (map, startPosition, target) => {
     }
   }
   return result;
-}
+};
 
 // ----------------------------------------------------------------------------
 // HELPER
@@ -88,56 +88,207 @@ const search = (map, startPosition, target) => {
 const isLowerCase = a => a === a.toLowerCase();
 const isUpperCase = a => !isLowerCase(a);
 
-const linesToMap = lines => lines.map(line => line.split(""));
+const linesToMap = lines => lines.map(line => line.split(''));
+
+// add item to the list and sort it by the distance (item -> [distance, data]
+const enqueue = (queue, element) => {
+  queue.push(element);
+  queue.sort((a, b) => {
+    const [da, ...x] = a;
+    const [db, ...y] = b;
+    return a > b;
+  });
+
+  return queue;
+};
+
+// get the first item of the queue
+const dequeue = queue => {
+  return queue.shift();
+};
+
+const createGraph = (start) => {
+  const m = new Map();
+  m.set(start, []);
+  return [[start], m];
+};
+
+const addNodeToGraph = (graph, node) => {
+  const [nodes, neighbours] = graph;
+  nodes.push(node);
+  neighbours.set(node, []);
+  return [nodes, neighbours];
+}
+
+const addEdgeToGraph = (graph, node1, node2, weight) => {
+  const [nodes, neighbours] = graph;
+  const neighbours1 = neighbours.get(node1);
+  const neighbours2 = neighbours.get(node2);
+  neighbours1.push([weight, node2]);
+  neighbours2.push([weight, node1]);
+  neighbours.set(node1, neighbours1);
+  neighbours.set(node2, neighbours2);
+  return [nodes, neighbours];
+}
+
+// graph -> [[nodes], map<node, [nodes]>]
+const djikstra = (graph, start, end) => {
+  let times = new Map();
+  let backtrace = new Map();
+  let queue = [];
+
+  times.set(start, 0);
+  let [nodes, neighbours] = graph;
+  nodes = nodes.map(node => {
+    if (node !== start) times.set(node, Infinity);
+  });
+
+  enqueue(queue, [0, start]);
+  while (queue.length > 0) {
+    let [distance, node] = dequeue(queue);
+    neighbours.get(node).forEach(([weight, neighbour]) => {
+      let time = times.get(node) + weight;
+      if (time < times.get(neighbour)) {
+        times.set(neighbour, time);
+        backtrace.set(neighbour, node);
+        enqueue(queue, [time, neighbour]);
+      }
+    });
+  }
+
+  let path = [end];
+  let lastStep = end;
+  while (lastStep !== start) {
+    path.unshift(backtrace.get(lastStep));
+    lastStep = backtrace.get(lastStep);
+  }
+
+  return [path, times.get(end)];
+};
 
 // ----------------------------------------------------------------------------
 // PART 1
 // ----------------------------------------------------------------------------
 const getKey = a => a.toLowerCase();
 const getDoor = a => a.toUpperCase();
-const nextKey = list => {
+
+const matchKeyDoors = (keys, doors) => {
+  let matched = new Map();
+  let unmatched = [];
+  for (let [key, keyPosition] of keys.entries()) {
+    if (doors.has(key.toUpperCase())) {
+      matched.set(key, [true, keyPosition]);
+    } else {
+      unmatched.push([key, keyPosition]);
+    }
+  }
+  return [matched, unmatched];
+};
+
+const doorsOnPath = path => {
+  return path.filter(item => isUpperCase(item))
 }
 
+const keysOnPath = path => {
+  return path.filter(item => isLowerCase(item))
+}
+
+const buildGraph = (map, [startPosition, keys, doors]) => {
+  let graph = createGraph("@");
+
+  // create a dependency list between keys and doors
+  const [matched, unmatched] = matchKeyDoors(keys, doors);
+  // there should be one key without a matching door
+  const [ukey, upos] = unmatched[0];
+  // search the map from the startPosition to the one and only key
+  const [x, y, count, foundItems] = search(map, startPosition, ukey);
+
+  // add all keys and doors as nodes to the graph
+  keys.forEach(([_,position], key) => {
+    graph = addNodeToGraph(graph, key);
+  });
+  doors.forEach(([_,position], key) => {
+    graph = addNodeToGraph(graph, key);
+  });
+
+  // add cost between a door and its key
+  matched.forEach(([_,position], key) => {
+    const  [x, y, distance, foundItems] = search(map, position, key.toUpperCase());
+    graph = addEdgeToGraph(graph, key, key.toUpperCase(), distance);
+  });
+
+  const directDoorsToTheOnekey = doorsOnPath(foundItems);
+  directDoorsToTheOnekey.forEach(door => {
+    const [dx, dy, dcount, dFoundItems] = search(map, [x, y], door);
+    const [kx, ky, kcount, kFoundItems] = search(map, [dx, dy], door.toLowerCase());
+    graph = addEdgeToGraph(graph, door.toLowerCase(), door, kcount);
+  });
+
+  console.log(graph)
+};
+
 const findData = map => {
-  let startPosition = [0,0];
+  let startPosition = [0, 0];
   let keys = new Map();
   let doors = new Map();
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[0].length; x++) {
       const v = map[y][x];
       if (v === '#' || v === '.') continue;
-      else if(v === '@') startPosition = [x,y];
-      else if(isLowerCase(v)) keys.set(v, [x,y]); 
-      else if(isUpperCase(v)) doors.set(v, [x,y]); 
+      else if (v === '@') startPosition = [x, y];
+      else if (isLowerCase(v)) keys.set(v, [x, y]);
+      else if (isUpperCase(v)) doors.set(v, [x, y]);
     }
   }
   return [startPosition, keys, doors];
 };
 
-const part1 = lines => {
-};
+const part1 = lines => {};
 
 // ----------------------------------------------------------------------------
 // PART 2
 // ----------------------------------------------------------------------------
-const part2 = lines => {
-};
+const part2 = lines => {};
 
 // ----------------------------------------------------------------------------
 // TESTS
 // ----------------------------------------------------------------------------
 const test = () => {
-  const lines = [
-    "#########",
-    "#b.A.@.a#",
-    "#########",
-  ];
+  const lines = ['#########', '#b.A.@.a#', '#########'];
   const map = linesToMap(lines);
   const [startPosition, keys, doors] = findData(map);
-  console.log(search(map, startPosition, 'b'));
+  let graph = buildGraph(map, [startPosition, keys, doors]);
 
   return false;
 };
+
+const testDjikstra = () => {
+  console.log("TEST DJIKSTRA");
+  let graph = createGraph("DIG INN");
+  graph = addNodeToGraph(graph, "FULLSTACK");
+  graph = addNodeToGraph(graph, "STARBUCKS");
+  graph = addNodeToGraph(graph, "DUBLINER");
+  graph = addNodeToGraph(graph, "CAFE GRUMPY");
+  graph = addNodeToGraph(graph, "INSOMNIA COOKIES");
+  graph = addEdgeToGraph(graph, "DIG INN", "FULLSTACK", 7);
+  graph = addEdgeToGraph(graph, "DIG INN", "DUBLINER", 4);
+  graph = addEdgeToGraph(graph, "DIG INN", "CAFE GRUMPY", 9);
+  graph = addEdgeToGraph(graph, "CAFE GRUMPY", "INSOMNIA COOKIES", 5);
+  graph = addEdgeToGraph(graph, "INSOMNIA COOKIES", "DUBLINER", 7);
+  graph = addEdgeToGraph(graph, "INSOMNIA COOKIES", "STARBUCKS", 6);
+  graph = addEdgeToGraph(graph, "DUBLINER", "STARBUCKS", 3);
+  graph = addEdgeToGraph(graph, "DUBLINER", "FULLSTACK", 2);
+  graph = addEdgeToGraph(graph, "FULLSTACK", "STARBUCKS", 6);
+  {
+  const [path, time] = djikstra(graph, "FULLSTACK", "INSOMNIA COOKIES");
+  console.log("should be 9", path, time);
+  }
+  {
+  const [path, time] = djikstra(graph, "DIG INN", "STARBUCKS");
+  console.log("should be 7", path, time);
+  }
+}
+
 
 const main = lines => {
   if (test()) {
